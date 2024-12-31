f7de4dc6eed745a1d35cb524f73784a79dfdd74d
The Phantom Technology -Advanced Pentest <155259033+The-Phantom-Technology@users.noreply.github.com>
2024-12-31 17:46:06 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -5,12 +5,13 @@ rm -rf /opt/PHANTX/sqlmap/.git 2>/dev/null
 rm -rf /opt/PHANTX/sqlmap 2>/dev/null
 
 if git clone --depth 1 https://github.com/sqlmapproject/sqlmap /opt/PHANTX/sqlmap 2>/dev/null; then
-  echo "Copy PACKAGE... PASS!"
+  echo -e "\033[32mCopy PACKAGE... PASS!\033[0m"
+
 else
   exit 1
 fi
 
-cp -Rf /opt/PHANTX/sqlmap/* /opt/PHANTX/bin/ 2>/dev/null
+cp -Rf /opt/PHOS/tool/sqlmap/phantxbin/* /opt/PHANTX/bin/ 2>/dev/null
 
 chmod -R 755 /opt/PHANTX/bin/ 2>/dev/null
 
