823cfb0d1fc0e0186358328a3b70b426be34b99e
The Phantom Technology -Advanced Pentest <155259033+thephantomtechnology@users.noreply.github.com>
2025-01-26 17:04:54 +0800

    更新 PHOSBUILD.sh

--- PHOSBUILD.sh
+++ PHOSBUILD.sh
@@ -1,7 +1,19 @@
 rm -rf /opt/PHANTX/sqlmap
 
-cp -Rf phantxbin/* /opt/PHANTX/bin/
-
-chmod -R 755 /opt/PHANTX/bin/
+rm -rf .git*
 
 cp -Rf $(pwd) /opt/PHANTX/sqlmap
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+cp -Rf phantxbin/* /opt/PHANTX/bin
+
+chown -R phantx:phantx /opt/PHANTX
+chmod -R 755 /opt/PHANTX
